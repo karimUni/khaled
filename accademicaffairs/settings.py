2b8025397a68bd796be9e00cb829056dd6fcdcbe
karimUni <karim@unilearn.com.eg>
2020-10-13 16:46:30 +0200

    removing migrations file & set database settings

--- accademicaffairs/settings.py
+++ accademicaffairs/settings.py
@@ -104,22 +104,22 @@ WSGI_APPLICATION = 'accademicaffairs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'unilearn_testback',
-#         'USER': 'unilearn_unilearn',
-#         'PASSWORD': 'unilearn@22',
-#         'HOST': 'localhost',
-#         'PORT': '3306'
-#     }
-# }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+ DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': 'unilearn_testback',
+         'USER': 'unilearn_unilearn',
+         'PASSWORD': 'unilearn@22',
+         'HOST': 'localhost',
+         'PORT': '3306'
+     }
+ }
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 
 # Password validation
